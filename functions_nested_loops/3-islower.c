73eb277a8449e74cf535d58e6615f2484fd225fe
JWebb-netizen <13069@holbertonstudents.com>
2026-03-07 13:55:55 +1100

    functions_nested_loops

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
-int _islower(int c)
+int _islower(char c)
 {
 if (c >= 0x61 && c <= 0x7A)
 {
-_putchar(1);
+_putchar('1');
 }
 else
 {
-_putchar(0);
+_putchar('0');
 }
 return (0);
 }
