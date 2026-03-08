984c9fe74aa00102f3fea16b5c43824c87b8ca11
JWebb-netizen <13069@holbertonstudents.com>
2026-03-08 15:54:19 +1100

    functions_nested_loops

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int print_last_digit(int r)
+{
+
+  _putchar(r + '0');
+  return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -7,4 +7,7 @@ int print_alphabet_x10(void);
 int _islower(char c);
 int _isalpha(char c);
 int print_sign(int n);
+int _abs(int r);
+int print_last_digit(int r);
+
 #endif
