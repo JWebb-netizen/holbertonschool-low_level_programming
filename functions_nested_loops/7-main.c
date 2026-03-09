11f1d9c6ac561f51e8abd88deccd76b7edd4a899
JWebb-netizen <13069@holbertonstudents.com>
2026-03-09 12:26:45 +1100

    functions_nested_loops

--- functions_nested_loops/7-main.c
+++ functions_nested_loops/7-main.c
@@ -1,11 +1,14 @@
 #include "main.h"
-
+#include <limits.h>
+#include <stdio.h>
 int main(void)
 {
 int r;
 
 print_last_digit(98);
 print_last_digit(0);
+print_last_digit(INT_MIN);
+print_last_digit(INT_MAX);
 r = print_last_digit(-1024);
 _putchar('0' + r);
 _putchar('\n');
--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -3,26 +3,28 @@
 int print_last_digit(int r)
 {
 if (r < 0)
-r = r * -1;
-if (r % 10 == 0)
+r = (r + 1) * -1;
+else
+r = r - 1;
+if (r % 10 + 1 == 0)
 _putchar('0');
-else if (r % 10 == 1)
+else if (r % 10 + 1 == 1)
 _putchar('1');
-else if (r % 10 == 2)
+else if (r % 10 + 1 == 2)
 _putchar('2');
-else if (r % 10 == 3)
+else if (r % 10 + 1 == 3)
 _putchar('3');
-else if (r % 10 == 4)
+else if (r % 10 + 1 == 4)
 _putchar('4');
-else if (r % 10 == 5)
+else if (r % 10 + 1 == 5)
 _putchar('5');
-else if (r % 10 == 6)
+else if (r % 10 + 1 == 6)
 _putchar('6');
-else if (r % 10 == 7)
+else if (r % 10 + 1 == 7)
 _putchar('7');
-else if (r % 10 == 8)
+else if (r % 10 + 1 == 8)
 _putchar('8');
-else if (r % 10 == 9)
+else if (r % 10 + 1 == 9)
 _putchar('9');
-return (r % 10);
+ return (r % 10 + 1);
 }
