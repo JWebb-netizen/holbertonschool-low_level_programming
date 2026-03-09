e49e6732d277c3b5cac6b4e6508367ec2dcbaf04
JWebb-netizen <13069@holbertonstudents.com>
2026-03-09 20:53:45 +1100

    more_functions_nested_loops

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+void print_diagonal(int n)
+{
+int i = 0, x = 0;
+if (n == 0)
+;
+else
+{
+for (i = 0; i < n; i++)
+{
+x = i;
+while (x != 0)
+{
+_putchar(' ');
+x--;
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+_putchar('\n');
+return;
+}
