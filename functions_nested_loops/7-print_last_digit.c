4a3e78624a92c2942121fe0a2c7835e8caf69c9c
JWebb-netizen <13069@holbertonstudents.com>
2026-03-09 11:41:25 +1100

    functions_nested_loops

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,27 @@
 
 int print_last_digit(int r)
 {
-
-  _putchar(r + '0');
-  return (0);
+if (r < 0)
+r = r * -1;
+if (r % 10 == 0)
+_putchar('0');
+else if (r % 10 == 1)
+_putchar('1');
+else if (r % 10 == 2)
+_putchar('2');
+else if (r % 10 == 3)
+_putchar('3');
+else if (r % 10 == 4)
+_putchar('4');
+else if (r % 10 == 5)
+_putchar('5');
+else if (r % 10 == 6)
+_putchar('6');
+else if (r % 10 == 7)
+_putchar('7');
+else if (r % 10 == 8)
+_putchar('8');
+else if (r % 10 == 9)
+_putchar('9');
+return (r % 10);
 }
