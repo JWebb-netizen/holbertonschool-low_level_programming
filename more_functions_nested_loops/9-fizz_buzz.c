41467b2a693f639dcd30882178c25f96be760a7e
JWebb-netizen <13069@holbertonstudents.com>
2026-03-09 21:34:07 +1100

    more_functions_nested_loops

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,9 @@ for (i = 1; i <= 100; i++)
 {
 a = i % 3;
 b = i % 5;
-if (a == 0 && b == 0)
+if (i == 100)
+printf("Buzz");
+else if (a == 0 && b == 0)
 printf("FizzBuzz ");
 else if (a == 0)
 printf("Fizz ");
